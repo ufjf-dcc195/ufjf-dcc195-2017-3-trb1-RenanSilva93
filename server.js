62bb7a3d7accb3a6c4738d4ba631800c4fb807b9
Renan Silva <renan.costas@hotmail.com>
2017-10-01 12:04:40 -0300

    heroku

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var http = require("http");
 var url = require("url");
+var porta = process.env.PORT || 3000;
 
 function start(router, handlers) {
   http.createServer(function (req, res) {
     var urlParsed = url.parse(req.url, true);
     console.log("Requisição recebida em"+(new Date()), "no caminho ", urlParsed.pathname);
     router.route(urlParsed.pathname, handlers, req, res);
-}).listen(3000);
+}).listen(porta);
 console.log("Servidor rodando em http://localhost:3000");
 }
 
